6e76a0e3e90733602eee5372f645642cf256e8d9
Kartik <iamkaranmishra551@gmail.com>
2025-04-03 21:16:42 +0530

    Add homepage view and URL routing for courses app

--- src/courses/views.py
+++ src/courses/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def homepage_view(request):
+    return HttpResponse("<h1 style='color:blue;'>Django Course Project.</h1>")
