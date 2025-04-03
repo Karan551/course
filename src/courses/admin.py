92f397461c642fab30ecba45c693afd03f9c2e08
Kartik <iamkaranmishra551@gmail.com>
2025-04-03 08:51:54 +0530

    Customize Django Admin To Render HTML images. Part-5 completed

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 from .models import Course
+from django.utils.html import format_html
+
 # Register your models here.
 
-admin.site.register(Course)
+admin.site.site_header = "Course Admin Panel"
+admin.site.index_title = "Course Site administration"
+admin.site.site_title = "Admin Panel"
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["title", "status", "access"]
+    list_filter = ["status", "access"]
+
+    fields = ["title", "description", "status",
+              "image", "access", "display_image"]
+
+    readonly_fields = ["display_image"]
+
+    def display_image(self, obj, *args, **kwargs):
+        url = obj.image.url
+
+        return format_html(f"<img src={url} alt={obj.title}-img>")
+
+    display_image.short_description = "Current Image"
