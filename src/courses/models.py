ebe33c599a18f34851c651c60bd75b8bbb4c981f
Kartik <iamkaranmishra551@gmail.com>
2025-04-05 19:25:07 +0530

    Enhance Course and Lesson models with display name and URL path methods, and update CloudinaryField configurations for better media management.

--- src/courses/models.py
+++ src/courses/models.py
@@ -24,7 +24,7 @@ class PublishStatus(models.TextChoices):
 
 
 def genertate_public_id(instance, *args, **kwargs):
-    title = instance.title 
+    title = instance.title
     unique_id = str(uuid.uuid4()).replace("-", "")
     if not title:
         return unique_id
@@ -36,24 +36,52 @@ def genertate_public_id(instance, *args, **kwargs):
 
 def get_display_name(instance, *args, **kwargs):
     print("this is instance::", instance)
-    title = instance.title
-    if title:
-        return title
-    return "Course upload"
+
+    if hasattr(instance, "get_display_name"):
+        return instance.get_display_name()
+
+    elif hasattr(instance, "title"):
+        return instance.title
+
+    model_class = instance.__class__
+    model_name = model_class.__name__
+
+    return f"{model_name} Course upload"
 
 
 def get_public_id_prefix(instance, *args, **kwargs):
-    title = instance.title
+    if hasattr(instance, "path"):
+        path = instance.path
+        if path.startswith("/"):
+            path = path[1:]
+        if path.endswith("/"):
+            path = path[:-1]
 
-    if title:
-        slug = slugify(title)
-        unique_id = str(uuid.uuid4()).replace("-", "")[:5]
-        return f"course/{slug}-{unique_id}"
+        return path
 
-    if instance.id:
-        return f"course/{instance.id}"
+    public_id = instance.public_id
 
-    return "Course Uploaded"
+    model_class = public_id.__class__
+    print("this is model name::", model_class)
+    model_name = model_class.__name__
+    model_name_slug = slugify(model_name)
+
+    if not public_id:
+        return f"{model_name_slug}"
+
+    return f"{model_name_slug}/{public_id}"
+
+    # title = instance.title
+
+    # if title:
+    #     slug = slugify(title)
+    #     unique_id = str(uuid.uuid4()).replace("-", "")[:5]
+    #     return f"course/{slug}-{unique_id}"
+
+    # if instance.id:
+    #     return f"course/{instance.id}"
+
+    # return "Course Uploaded"
 
 
 def handle_upload(instance, file_name):
@@ -88,6 +116,16 @@ class Course(models.Model):
             self.public_id = genertate_public_id(self)
 
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return self.path
+
+    @property
+    def path(self):
+        return f"/courses/{self.public_id}"
+
+    def get_display_name(self):
+        return f"{self.title} - Course"
 
     @property
     def is_published(self):
@@ -152,9 +190,19 @@ class Lesson(models.Model):
     description = models.TextField(blank=True, null=True)
     public_id = models.CharField(max_length=150, null=True, blank=True)
     order = models.IntegerField(default=0)
-    thumbnail = CloudinaryField("image", null=True, blank=True)
-    video = CloudinaryField("video", blank=True,
-                            null=True, resource_type="video")
+    thumbnail = CloudinaryField("image",
+                                public_id_prefix=get_public_id_prefix,
+                                display_name=get_display_name,
+                                tags=["thumbnail", "lesson"],
+                                null=True,
+                                blank=True)
+    video = CloudinaryField("video",
+                            blank=True,
+                            null=True,
+                            public_id_prefix=get_public_id_prefix,
+                            display_name=get_display_name,
+                            tags=["video", "lesson"],
+                            resource_type="video")
     can_preview = models.BooleanField(default=False,
                                       help_text="If user does not have access this course.Can they see this ?")
     status = models.CharField(
@@ -168,6 +216,18 @@ class Lesson(models.Model):
             self.public_id = genertate_public_id(self)
 
         super().save(*args, **kwargs)
+
+    @property
+    def path(self):
+        course_path = self.course.path
+
+        if course_path.endswith("/"):
+            course_path = course_path[:-1]
+
+        return f"{course_path}/lesson/{self.public_id}"
+
+    def get_display_name(self):
+        return f"{self.title}-{self.course.get_display_name()}"
 
     class Meta:
         ordering = ["order", "-updated_at"]
